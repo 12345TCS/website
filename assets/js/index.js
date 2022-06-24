d1ebeb8f9e001035b7ce4418dfae332a60c6ac20
DezyNation <87603422+DezyNation@users.noreply.github.com>
2022-06-24 18:35:24 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,12 +9,21 @@ window.addEventListener("load", ()=>{
 */
 
 
-function openForm(){
-    form.style.left = "0";
-}
 function closeForm(){
     form.style.left = "100%";
+    formclicks = 0;
 }
+
+function openForm(){
+    if(formclicks==0){
+        form.style.left = "0";
+        formclicks = 1;
+    }
+    else{
+        closeForm();
+    }
+}
+
 
 function showAlert(){
     window.alert("We've received your details and we'll contact you soon.");
